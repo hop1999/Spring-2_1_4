0fd7708581c2d52a9ad08d191a883c497429c77c
hop1999 <cosmik9@mail.ru>
2023-05-22 03:45:40 +0300

    4
    4

---
+++ src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,9 @@
+package koschei.models;
+
+import org.springframework.stereotype.Service;
+
+@Service("De(a)thBean")
+public class Deth8 {
+    @Override
+    public String toString() {return "Gangsta paradise bratishka...";}
+}
---
+++ src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,15 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("NeedleBean")
+public class Needle7 {
+    private final Deth8 deth;
+
+    @Autowired
+    public Needle7(Deth8 deth) {this.deth = deth;}
+
+    @Override
+    public String toString() {return ", смерть Кощея на игле :( " + deth.toString();}
+}
